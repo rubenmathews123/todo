36a20ae271ef6995a041db6b02d33aa1845fb476
rubenmathews123 <98581262+rubenmathews123@users.noreply.github.com>
2022-08-01 12:18:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Form from './components/Form';
+import Todo from './components/Todo';
 import './App.css';
 
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+
+  const [formInput, setFormInput] = useState('');
+
+  const handleChange = (e) => {
+    setFormInput(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (formInput !== '') {
+      const date = new Date().toLocaleDateString()
+      const newTask = {
+        date: date,
+        task: formInput,
+        completed: false
+      }
+      setTasks([...tasks, newTask])
+      setFormInput('')
+    }
+  }
+
+  const handleComplete = (index) => {
+    const newTasks = [...tasks]
+    if (newTasks[index].completed === false) {
+      newTasks[index].completed = true
+    } else {
+      newTasks[index].completed = false
+    }
+    setTasks(newTasks)
+  }
+
+  const handleRemove = (index) => {
+    const newTasks = [...tasks]
+    newTasks.splice(index, 1)
+    setTasks(newTasks)
+  }
+
+  const handleRemoveAll = () => {
+    setTasks([])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Form formInput={formInput} handleChange={handleChange} handleSubmit={handleSubmit} />
+      <Todo tasks={tasks} handleComplete={handleComplete} handleRemove={handleRemove} handleRemoveAll={handleRemoveAll} />
+
     </div>
   );
 }
